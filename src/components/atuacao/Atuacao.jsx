2299f2bcd16247645af7975964e7739946212d7b
Peu <peustratt@gmail.com>
2022-01-04 02:17:36 -0300

    Criado atuação

--- src/components/atuacao/Atuacao.jsx
+++ src/components/atuacao/Atuacao.jsx
@@ -1,10 +1,43 @@
 import './atuacao.scss'
+import {AccountBalanceTwoTone} from '@material-ui/icons/';
 
 
 function Atuacao() {
     return (
         <div className='atuacao'>
-            
+            <div className="content-wrapper">
+                <h2 className="atuacao__title">Atuação</h2>
+                <ul className="cards">
+                    <li className="card">
+                        <AccountBalanceTwoTone className="card__icon"/>
+                        <h3 className="card__title">Tributário</h3>
+                    </li>
+                    <li className="card">
+                        <AccountBalanceTwoTone className="card__icon"/>
+                        <h3 className="card__title">Tributário</h3>
+                    </li>
+                    <li className="card">
+                        <AccountBalanceTwoTone className="card__icon"/>
+                        <h3 className="card__title">Tributário</h3>
+                    </li>
+                    <li className="card">
+                        <AccountBalanceTwoTone className="card__icon"/>
+                        <h3 className="card__title">Tributário</h3>
+                    </li>
+                    <li className="card">
+                        <AccountBalanceTwoTone className="card__icon"/>
+                        <h3 className="card__title">Tributário</h3>
+                    </li>
+                    <li className="card">
+                        <AccountBalanceTwoTone className="card__icon"/>
+                        <h3 className="card__title">Tributário</h3>
+                    </li>
+                    <li className="card">
+                        <AccountBalanceTwoTone className="card__icon"/>
+                        <h3 className="card__title">Tributário</h3>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
