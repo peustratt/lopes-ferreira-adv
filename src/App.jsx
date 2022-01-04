5b6d66b0f680ca6b9990d246ad7176d80a03d38e
Peu <peustratt@gmail.com>
2022-01-04 15:36:23 -0300

    add whatsapp icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './app.scss'
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
 import Navbar from './components/navbar/Navbar';
 import Intro from './components/intro/Intro';
 import Atuacao from './components/atuacao/Atuacao';
@@ -12,6 +13,7 @@ function App() {
     <main className='app'>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <WhatsAppIcon className='whats-icon' />
         <section className="sections">
             <Intro />
             <Atuacao />
