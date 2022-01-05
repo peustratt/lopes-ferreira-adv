557bbe972fe577eb735495925ebf1de7a9d6c883
Peu <peustratt@gmail.com>
2022-01-05 20:13:14 -0300

    styled whatsapp icon

--- src/components/atuacao/Atuacao.jsx
+++ src/components/atuacao/Atuacao.jsx
@@ -1,44 +1,65 @@
+import React from 'react';
 import './atuacao.scss'
-import {AccountBalanceTwoTone, Work, Gavel, MonetizationOn} from '@material-ui/icons/';
+import { AccountBalanceTwoTone, Work, Gavel, MonetizationOn } from '@material-ui/icons/';
 import CardAtuacao from '../card-atuacao/CardAtuacao';
 
 const cardsData = [
     {
         id: 1,
         Icon: AccountBalanceTwoTone,
-        title: "tributário"
+        title: "tributário",
+        on: false
     },
     {
         id: 2,
         Icon: MonetizationOn,
-        title: "empresarial"
+        title: "empresarial",
+        on: false
     },
     {
         id: 3,
         Icon: Gavel,
-        title: "trabalhista"
+        title: "trabalhista",
+        on: false
     },
     {
         id: 4,
         Icon: Work,
-        title: "internacional"
+        title: "internacional",
+        on: false
     }
 
 ]
 
 function Atuacao() {
-    const cardsJSX = cardsData.map((card) => {
-        return <CardAtuacao key={card.id} Icon={card.Icon} title={card.title} />
+    const [cards, setCards] = React.useState(cardsData)
+
+    function toggleCard(id) {
+        setCards((prevState) => {
+            return prevState.map((card) => {
+                return card.id === id ? { ...card, on: !card.on } : card
+            })
+        })
+    }
+
+    const cardsJSX = cards.map((card) => {
+        return <CardAtuacao
+            key={card.id}
+            Icon={card.Icon}
+            title={card.title}
+            on={card.on}
+            toggleCard={() => toggleCard(card.id)}
+            />
     })
 
     return (
         <div className='atuacao' id="atuacao">
 
-                <h2 className="atuacao__title">Atuação</h2>
-                <ul className="cards">
-                    {cardsJSX}
-                </ul>
-                
+            <h2 className="atuacao__title">Atuação</h2>
+            <ul className="cards">
+                {cardsJSX}
+            </ul>
+
         </div>
     )
 }
--- src/components/card-atuacao/CardAtuacao.jsx
+++ src/components/card-atuacao/CardAtuacao.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './card-atuacao.scss'
 
-function CardAtuacao({title, Icon}) {
+function CardAtuacao({title, Icon, toggleCard}) {
     return (
-        <li className="card-atuacao">
+        <li className="card-atuacao" onClick={toggleCard}>
             <Icon className="card__icon" />
             <h3 className="card__title">{title}</h3>
         </li>
