aff9ed06042b7aacb5dde8decf05c69309b14bf4
Peu <peustratt@gmail.com>
2022-01-05 23:58:21 -0300

    componentizado list-itens

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,28 +1,43 @@
 import React from 'react'
 import './menu.scss'
 
+const navData = [
+    {
+        name: "Inicio",
+        link: "#intro"
+    },
+    {
+        name: "Atuação",
+        link: "#atuacao"
+    },
+    {
+        name: "Equipe",
+        link: "#equipe"
+    },
+    {
+        name: "Contato",
+        link: "#contato"
+    }
+]
+
 function Menu(props) {
+    const navListJSX = navData.map((item) => {
+        return (
+            <li className="nav__item">
+                <a
+                    href={item.link}
+                    className='nav__link'
+                    onClick={() => props.setMenuOpen(false)}
+                >   {item.name}
+                </a>
+            </li>
+        )
+    })
+
     return (
         <div className={"menu " + (props.menuOpen && "active")}>
             <ul className="nav__list">
-                <li className="nav__item">
-                    <a href="#intro" className="nav__link" onClick={() => props.setMenuOpen(false)}>Inicio</a>
-                </li>
-                <li className="nav__item">
-                    <a href="#atuacao" className="nav__link" onClick={() => props.setMenuOpen(false)}>Atuação</a>
-                </li>
-                <li className="nav__item">
-                    <a href="#equipe" className="nav__link" onClick={() => props.setMenuOpen(false)}>Equipe</a>
-                </li>
-                <li className="nav__item">
-                    <a href="#sobre" className="nav__link" onClick={() => props.setMenuOpen(false)}>Sobre</a>
-                </li>
-                <li className="nav__item">
-                    <a href="#equipe" className="nav__link" onClick={() => props.setMenuOpen(false)}>Blog</a>
-                </li>
-                <li className="nav__item">
-                    <a href="#contato" className="nav__link" onClick={() => props.setMenuOpen(false)}>Contato</a>
-                </li>
+                {navListJSX}
             </ul>
         </div>
     )
