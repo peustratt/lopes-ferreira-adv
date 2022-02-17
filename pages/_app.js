6c3f8f7e46d423721b8e2e1e218c547bd88a58c3
Peu <peustratt@gmail.com>
2022-02-17 17:40:41 -0300

    SSR fix try 01

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,6 @@
 import { createGlobalStyle, ThemeProvider} from "styled-components"
-import { useEffect } from 'react';
 
 export default function MyApp({ Component, pageProps }) {
-
-  useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
   return (
     <>
       <GlobalStyle />
